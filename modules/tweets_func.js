f90f01bea19010d21fa468c94f1db5bae4ea1145
Brice WeKeys <formation@wekeys.fr>
2026-04-14 18:50:41 +0200

    Backend - I like this tweet

---
+++ modules/tweets_func.js
@@ -0,0 +1,34 @@
+const Trend = require('../models/trends');
+const Tweet = require('../models/tweets');
+
+function getTrendsFromTweet(string) {
+  //extrait dans un tableau les mots dièses contenus dans un tweet exemple ['#Trends','#France','#Foot2Rue']
+  const myRegex = new RegExp(/\#[\w\d\-\@\é\à\è\ù\ç\û\&]*/,'ig');
+  let myExtractedTrends = [];
+  [...string.matchAll(myRegex)].forEach((trend) => myExtractedTrends.push(trend[0].toLowerCase()))
+  return myExtractedTrends;
+};
+
+function sendTrends(array) {
+  //envoie sur la collection MongoDB trends les différents mots dièses contenus dans un tweet
+  array.forEach(async (trend) => {
+    const isTrend = await Trend.findOne({hashtags: trend}).exec();
+    if (isTrend === null) { //Si la trend n'existe pas, on la créée
+      const newTrend = new Trend ({
+        hashtags: trend,
+        count: 1
+      })
+      newTrend.save()
+    } else { //Si la trend existe, on la modifie pour augmenter le compteur
+      await Trend.updateOne({hashtags: trend}, { count: isTrend.count+1}).then()
+    }
+  })
+}
+//Ajouter dans le tweet les IDs
+function updateTrendsAndTweet(numbertweet, arrayTrends) {
+  arrayTrends.forEach(async(trend) => {
+    const myTrendId = await Trend.findOne({hashtags: trend}).exec();
+    await Tweet.updateOne({_id: numbertweet}, {$push: {hashtags: myTrendId._id}}).then()
+  })
+}
+module.exports = { updateTrendsAndTweet, sendTrends, getTrendsFromTweet};
