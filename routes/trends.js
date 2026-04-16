362a37da25c4a2142fa3f415f411af8296a88734
Brice WeKeys <formation@wekeys.fr>
2026-04-17 00:28:41 +0200

    Backend - Ajout de la route pour obtenir les tweets de trend

--- routes/trends.js
+++ routes/trends.js
@@ -9,13 +9,20 @@ const { checkBody } = require('../modules/checkBody');
 
 // Obtiens les 5 meilleurs trends et les renvoies dans un tableau
 router.get('/besttrends', async (req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     await Trend.find({count:{$exists:true}}).sort({count:-1}).limit(5)
     .then(data => res.status(200).send({result: true, trends: data}))
 });
 
 // Affiche les X premiers tweets correspondant à la trend
-router.get('/trend/:trendid/:start/:stop', (req, res) => {
-
+router.get('/:trendid/:start/:stop', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+if (!checkBody(req.params, ['trendid', 'start', 'stop'])) {
+    res.status(200).send({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+  const batch = await Tweet.find({hashtags: req.params.trendid}).populate({path: "username", model: User, select: {avatar: 1, firstname: 1, username: 1}}).sort({date: -1}).skip(req.params.start).limit(req.params.stop);
+  return res.status(200).send({result: true, tweets: batch});
 })
 
 
